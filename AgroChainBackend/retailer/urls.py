ea953484e4cd175159d40310acdb7edb6b280e1f
Dipesh Rawal <dipeshrawalayush@gmail.com>
2024-11-30 19:44:32 +0545

    update1

--- AgroChainBackend/retailer/urls.py
+++ AgroChainBackend/retailer/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from retailer.views import GetAllRetailer, RetailerLoginView, RetailerRegistrationView
+from retailer.views import GetAllRetailer, RetailerLoginView, RetailerRegistrationView, RetailerProfileView
 
 
 urlpatterns = [
     path('register/',RetailerRegistrationView.as_view(),name='retailer_register'),
     path('login/',RetailerLoginView.as_view(),name='retailer_login'),
-    path('profile/',RetailerLoginView.as_view(),name='retailer_profile'),
+    path('profile/',RetailerProfileView.as_view(),name='retailer_profile'),
     path('retailer-all/',GetAllRetailer.as_view(),name='retailer_all'),
 ]
